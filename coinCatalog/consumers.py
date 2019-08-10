995e19921c6d80da6ba629f9b582c644dde7396f
Zimovik007 <programm00@ya.ru>
2019-08-10 16:07:06 +1000

    some refactoring

--- coinCatalog/consumers.py
+++ coinCatalog/consumers.py
@@ -2,21 +2,22 @@ import asyncio
 
 from channels.consumer import AsyncConsumer
 
+
 class StreamConsumer(AsyncConsumer):
-  
-  async def websocket_connect(self, event):
-    print('connection successful!!', event)
-    await self.send({
-      "type": "websocket.accept",
-    })
 
-    await self.send({
-      "type": "websocket.send",
-      "text": "hello, world!",
-    })
+    async def websocket_connect(self, event):
+        print('connection successful!!', event)
+        await self.send({
+            "type": "websocket.accept",
+        })
 
-  async def websocket_receive(self, event):
-    print("receive ", event)
+        await self.send({
+            "type": "websocket.send",
+            "text": "hello, world!",
+        })
 
-  async def websocket_disconnect(self, event):
-    print("disconnect ", event)
+    async def websocket_receive(self, event):
+        print("receive ")
+
+    async def websocket_disconnect(self, event):
+        print("disconnect ", event)
---
+++ coinCatalog/static/coinCatalog/js/websocket.js
@@ -0,0 +1,35 @@
+const video = document.querySelector('video');
+
+navigator.mediaDevices.getUserMedia({video: {width: 600, height: 600}}).then((stream) => video.srcObject = stream);
+
+const getFrame = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    return canvas.toDataURL('image/png');
+};
+const FPS = 3;
+
+let socketUrl = "ws://" + window.location.host + window.location.pathname;
+let socket = new WebSocket(socketUrl);
+
+socket.onmessage = function (event) {
+    console.log(event);
+};
+
+socket.onopen = function (event) {
+    console.log('socket opened');
+    console.log(event);
+    setInterval(() => socket.send(getFrame()), 1000 / FPS);
+};
+
+socket.onclose = function (event) {
+    console.log('socket closed');
+    console.log(event);
+};
+
+socket.onerror = function (event) {
+    console.log('socket errors');
+    console.log(event);
+};
